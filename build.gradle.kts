799b1ef983d6a9fdb8f6001a172ee6a5cb911b44
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-03-20 14:55:54 +0100

    [BIRT-3] infra: Initial package structure and Spring Boot stub

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
-    kotlin("jvm") version "2.3.0"
+    application
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.spring") version "2.3.10"
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("project-report")
 }
 
 group = "com.pretz"
@@ -10,7 +15,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation(kotlin("test-junit5"))
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.3")
 }
 
 kotlin {
