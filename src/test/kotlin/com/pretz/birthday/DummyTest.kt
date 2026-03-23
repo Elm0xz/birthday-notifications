950b7e14b94580e1ba34da2579784bd8df338a3f
Przemyslaw Etz <przemyslaw.etz@gmail.com>
2026-03-23 13:47:40 +0100

    [BIRT-22] infra: Add testing step with dummy test

---
+++ src/test/kotlin/com/pretz/birthday/DummyTest.kt
@@ -0,0 +1,12 @@
+package com.pretz.birthday
+
+import org.assertj.core.api.Assertions
+import kotlin.test.Test
+
+class DummyTest {
+
+    @Test
+    fun shouldDoDummyTest() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
